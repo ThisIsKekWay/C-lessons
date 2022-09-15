4227fdb697cdc723cbb6a8d827b6609439c77e45
Koha <BelkoZdorovo@yandex.ru>
2022-09-15 21:22:18 +0300

    Homework exercise 3

---
+++ Homehork_Ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Проверка числа на четность
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int Num = int.Parse(Console.ReadLine());
+
+if(Num % 2 == 0)
+    {
+        Console.WriteLine("Число четное");
+    }
+else
+    {
+        Console.WriteLine("Число нечетное");
+    }
