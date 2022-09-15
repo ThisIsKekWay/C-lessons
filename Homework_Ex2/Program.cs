73ba872998cfc7741b967d8e15553d982e9091e4
Koha <BelkoZdorovo@yandex.ru>
2022-09-15 22:10:54 +0300

    HWEx2

---
+++ Homework_Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое целое число");
+int firstNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int secondNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье целое число");
+int thirdNum = int.Parse(Console.ReadLine());
+
+int max = firstNum;
+
+if(secondNum > max) max = secondNum;
+if(thirdNum > max) max = thirdNum;
+
+Console.WriteLine("max = " + max);
