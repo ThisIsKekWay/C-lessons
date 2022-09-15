c1f3509ace8bf1e99297e4d9a8970f7fbfc71ed7
Koha <BelkoZdorovo@yandex.ru>
2022-09-15 18:53:03 +0300

    Example004 Dividing of two nums

---
+++ Example004_DividingTwoNums/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите первое целое число");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(firstNum / secondNum);
