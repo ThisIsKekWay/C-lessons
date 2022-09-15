8b724d9dd1ec46ac2eecb8e425bd1f6130fce041
Koha <BelkoZdorovo@yandex.ru>
2022-09-15 20:36:52 +0300

    Homework exercise 1

---
+++ Homework_Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.WriteLine("Введите первое целое число");
+int firstNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int secondNum = int.Parse(Console.ReadLine());
+
+if(firstNum > secondNum)
+    {
+        Console.WriteLine("Первое число больше второго");
+    }
+
+else
+    {
+        Console.WriteLine("Второе число больше первого");
+    }
