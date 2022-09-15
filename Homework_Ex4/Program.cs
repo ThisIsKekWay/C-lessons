36b052cb59a4940f5c387fe2b200432de71d5925
Koha <BelkoZdorovo@yandex.ru>
2022-09-15 22:16:40 +0300

    HWEx4

---
+++ Homework_Ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+
+if(num > 0)
+    {
+        int count = 2;
+        while(count <= num)
+            {
+                Console.Write($"{count} ");
+                count += 2;
+            }
+    }
+else
+    {
+        int count = -2;
+        while(count >= num)
+            {
+                Console.Write($"{count} ");
+                count -= 2;
+            }
+    }
