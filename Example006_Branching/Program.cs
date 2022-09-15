f9351af4ceba71943f6d05516789f055d80263d8
Koha <BelkoZdorovo@yandex.ru>
2022-09-15 19:07:44 +0300

    Example006 Branching operator

---
+++ Example006_Branching/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string Username = Console.ReadLine();
+
+if(Username == "Ирина")
+    {
+        Console.WriteLine("Здарова, пузяка");
+    }
+else
+    {
+        Console.WriteLine("Здарова, ты кто?");
+    }
