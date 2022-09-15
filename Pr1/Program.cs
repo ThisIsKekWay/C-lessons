fe3eca3775629a3cf66eaa1618c410a9320542b1
Koha <BelkoZdorovo@yandex.ru>
2022-09-15 22:08:11 +0300

    Practice

---
+++ Pr1/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Console.Clear();
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+int count = -num;
+
+while(count <= num)
+    {
+        Console.Write($"{count} ");
+        count ++;
+    }
+ */
+
+/* Console.WriteLine("Введите число");
+int Num = Math.Abs(int.Parse(Console.ReadLine()));
+
+
+if((Num < 1000) & (Num > 99))
+    {
+        Console.WriteLine(Num % 10);
+    }
+else
+    {
+        Console.WriteLine("Число не соответствует условию задачи"); 
+    }
+ */
