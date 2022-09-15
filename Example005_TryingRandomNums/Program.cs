c487330600b577dd56fe572556191e9dad151659
Koha <BelkoZdorovo@yandex.ru>
2022-09-15 18:58:25 +0300

    Example005 Trying some random

---
+++ Example005_TryingRandomNums/Program.cs
@@ -0,0 +1,5 @@
+﻿int firstNum = new Random().Next(0, 501);
+Console.WriteLine("Первое число равно " + firstNum);
+int secondNum = new Random().Next(0, 501);
+Console.WriteLine("Второе число равно " + secondNum);
+Console.WriteLine("Сумма равна " + (firstNum + secondNum));
