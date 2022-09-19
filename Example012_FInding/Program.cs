ab8e61e39e2d8c17aa0fd3d3fdf483f85d9c3f78
Koha <BelkoZdorovo@yandex.ru>
2022-09-19 10:04:45 +0300

    Finding needed num

---
+++ Example012_FInding/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {71, 62, 43, 14, 95, 86, 57, 38, 29};
+
+int n = array.Length;
+int find = 95;
+
+int index = 0;
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
