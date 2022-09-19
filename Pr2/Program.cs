3ed7620f3c0cc74cb76cc451c273359bab685b1e
Koha <BelkoZdorovo@yandex.ru>
2022-09-19 22:39:38 +0300

    Practice lesson #2

---
+++ Pr2/Program.cs
@@ -0,0 +1,56 @@
+﻿/* int num = new Random().Next(100, 1000);
+Console.WriteLine("Число равно " + num);
+
+int firstDigit = num / 100;
+int lastDigit = num % 10;
+
+Console.WriteLine(firstDigit * 10 + lastDigit);
+*/
+
+/*  
+Console.WriteLine("Введите первое целое число");
+int firstNum = Math.Abs(int.Parse(Console.ReadLine()));
+Console.WriteLine("Введите второе целое число");
+int secondNum = Math.Abs(int.Parse(Console.ReadLine()));
+
+if(firstNum % secondNum == 0)
+{
+    Console.WriteLine("Числа кратны");
+}
+else
+{
+    Console.WriteLine("Числа не кратны, остаток от деления равен " + firstNum % secondNum);
+}
+ */
+
+
+
+
+
+/* Console.WriteLine("Введите целое число");
+int num = Math.Abs(int.Parse(Console.ReadLine()));
+
+if((num % 7 == 0) & (num % 23 == 0))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+ */
+
+
+Console.WriteLine("Введите первое целое число");
+int firstNum = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе целое число");
+int secondNum = int.Parse(Console.ReadLine()!);
+
+if((Math.Pow(firstNum, 2) == secondNum) ^ (Math.Pow(secondNum, 2) == firstNum))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
