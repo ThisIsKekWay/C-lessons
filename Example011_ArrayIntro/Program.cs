d512ab4c136cc80d74429fcec8c8a5634b5e1f47
Koha <BelkoZdorovo@yandex.ru>
2022-09-19 09:35:24 +0300

    Max of 9 nums with array

---
+++ Example011_ArrayIntro/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {15, 21, 318, 415, 55, 61, 725, 82, 91};
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
