5fa6c5e43b1706c84ee479773969e9e45bc1455c
Koha <BelkoZdorovo@yandex.ru>
2022-09-19 22:06:01 +0300

    The third sign of a any-digit num

---
+++ HomeWork2_Ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+int Third(int number)
+{
+    int count = 0;
+    while(count < 2)
+    {
+        number = number / 10;
+        count += 1;        
+    }
+    int result = number % 10;
+    return result;
+}
+
+
+Console.WriteLine("Введите число");
+int num = Math.Abs(int.Parse(Console.ReadLine()));
+
+if(Third(num) == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(Third(num));
+}
