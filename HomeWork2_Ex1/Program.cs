3dc5c14e0738260077a56c0c91bf27eab3b78827
Koha <BelkoZdorovo@yandex.ru>
2022-09-19 20:31:58 +0300

    The second sign of a three-digit number

---
+++ HomeWork2_Ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int Second(int number)
+{
+    int result = ((number / 10) % 10);
+    return result;
+}
+
+
+Console.WriteLine("Введите число");
+int num = Math.Abs(int.Parse(Console.ReadLine()));
+
+
+if((num < 1000) & (num > 99))
+{
+    Console.WriteLine(Second(num));
+}
+else
+{
+    Console.WriteLine("Число не соответствует условию задачи"); 
+}
