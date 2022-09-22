f8de2db6aa8122ff9858fcb14c2de93334038d05
Koha <BelkoZdorovo@yandex.ru>
2022-09-22 20:53:19 +0300

    Abs deleted

--- Pr3/Program.cs
+++ Pr3/Program.cs
@@ -1,23 +1,23 @@
 ﻿Console.Clear();
 
 Console.Write("Введите число ");
-int num = Math.Abs(int.Parse(Console.ReadLine()!));
+int num = int.Parse(Console.ReadLine()!);
 
 switch(num)
 {
     case 1:
-    Console.WriteLine("x > 0, y > 0");
-    break;
+        Console.WriteLine("x > 0, y > 0");
+        break;
     case 2:
-    Console.WriteLine("x < 0, y > 0");
-    break;
+        Console.WriteLine("x < 0, y > 0");
+        break;
     case 3:
-    Console.WriteLine("x < 0, y < 0");
-    break;
+        Console.WriteLine("x < 0, y < 0");
+        break;
     case 4:
-    Console.WriteLine("x > 0, y < 0");
-    break;
+        Console.WriteLine("x > 0, y < 0");
+        break;
     default:
-    Console.WriteLine("Введено некорректное число");
-    break;
+        Console.WriteLine("Введено некорректное число");
+        break;
 }
