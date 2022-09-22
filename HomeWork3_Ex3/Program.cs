017cef21ddd1f5daf0f6305a1bf53286397b5f1e
Koha <BelkoZdorovo@yandex.ru>
2022-09-22 23:48:31 +0300

    HWEx3

---
+++ HomeWork3_Ex3/Program.cs
@@ -0,0 +1,36 @@
+﻿void AllTripple(int number)
+{
+    int count = 0;
+    if (number < 0)
+    {
+        count = -1;
+        while (count > number - 1)
+        {
+            Console.Write(Math.Pow(count, 3));
+            if (count != number)
+            {
+                Console.Write(", ");
+            }  
+            count --;
+        }
+    }
+    else
+    {
+        count = 1;
+        while (count < number + 1)
+        {
+            Console.Write(Math.Pow(count, 3));
+            if (count != number)
+            {
+                Console.Write(", ");
+            }  
+            count ++;
+        }
+    }
+}        
+        
+
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine()!);
+
+AllTripple(num);
