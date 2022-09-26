b7b46f91c7b334e926111a901c22d60809d80eba
Koha <BelkoZdorovo@yandex.ru>
2022-09-26 20:24:34 +0300

    HW4Ex2

---
+++ HomeWork4_Ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int SumOfDigits(int number)
+{
+    string l = Convert.ToString(number);
+    int[] array = new int[l.Length];
+    int sum = 0;
+    for (int i = 0; i < l.Length; i++)
+    {
+        array[i] = number % 10;
+        number = number / 10;
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+Console.Write("Введите число ");
+int num = Math.Abs(int.Parse(Console.ReadLine()!));
+Console.Write(SumOfDigits(num));
