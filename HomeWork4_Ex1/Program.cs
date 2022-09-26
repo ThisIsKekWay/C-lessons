f7c5831cc0ae86db0c91a384b4496420eb978329
Koha <BelkoZdorovo@yandex.ru>
2022-09-26 22:55:17 +0300

    HW4Ex1

--- HomeWork4_Ex1/Program.cs
+++ HomeWork4_Ex1/Program.cs
@@ -1,8 +1,18 @@
 ﻿Console.Clear();
 
+int Increase(int firstNum, int secondNum)
+{
+    int result = 1;
+    for(int i = 0; i < secondNum; i++)
+    {
+        result *= firstNum;
+    }
+    return result;
+}
+
 Console.WriteLine("Введите первое целое число");
 int firstNum = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе целое число");
-int secondNum = int.Parse(Console.ReadLine());
+int secondNum = Math.Abs(int.Parse(Console.ReadLine()));
 
-Console.WriteLine($"{firstNum} в степени {secondNum} = {Math.Pow(firstNum, secondNum)}");
+Console.WriteLine($"{firstNum} в степени {secondNum} = {Increase(firstNum, secondNum)}");
