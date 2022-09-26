65e27fd65e0c2ee15f46dba22cebf7bf95fc11cf
Koha <BelkoZdorovo@yandex.ru>
2022-09-26 20:06:54 +0300

    HW4Ex1

---
+++ HomeWork4_Ex1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое целое число");
+int firstNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int secondNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{firstNum} в степени {secondNum} = {Math.Pow(firstNum, secondNum)}");
