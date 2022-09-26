20643c7d889bbf58a3c049849e86bce0bb195a3c
Koha <BelkoZdorovo@yandex.ru>
2022-09-26 22:59:30 +0300

    HW4Ex3

--- HomeWork4_Ex3/Program.cs
+++ HomeWork4_Ex3/Program.cs
@@ -1,4 +1,29 @@
-﻿Console.Write("Введите числа через запятую: ");
+﻿/* Абсолютно бездарное решение, но работает.
+
+Console.Write("Введите числа через запятую: ");
 string[] array = Console.ReadLine()!.Split(", ");
 Console.WriteLine($" [{String.Join(", ", array)}] ");
+ */
 
+// Решено как полагается.
+
+int[] CreateArray()
+{
+    int[] array = new int [8];
+    return array;
+}
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next();
+        index ++;
+    }
+}
+
+int[] array = CreateArray();
+FillArray(array);
+Console.WriteLine(String.Join(", ", array));
