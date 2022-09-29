b849f9398e64ccffe41262a5ca654f278540e29a
Koha <BelkoZdorovo@yandex.ru>
2022-09-29 22:43:35 +0300

    HW5Ex2

---
+++ HomeWork5_Ex2/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] CreateArray()
+{
+    int[] array = new int [new Random().Next(2, 11)];
+    return array;
+}
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-100, 101);
+        index ++;
+    }
+}
+
+int SumOnOddPosition(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        result += array[i];
+    }
+
+    return result;
+}
+
+int[] array = CreateArray();
+FillArray(array);
+
+Console.WriteLine($"Сумма элементов на нечетных позициях в массиве {String.Join(", ", array)} равна {SumOnOddPosition(array)}");
