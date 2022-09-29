453ec34f4f3166a0d064337978d6ff52489bb00c
Koha <BelkoZdorovo@yandex.ru>
2022-09-29 22:32:22 +0300

    HW5Ex1

---
+++ HomeWork5_Ex1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateArray()
+{
+    int[] array = new int [new Random().Next(2, 11)];
+    return array;
+}
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-500, 501);
+        index ++;
+    }
+}
+
+int CountOdd(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) result++;
+    }
+    return result;
+}
+
+int[] array = CreateArray();
+FillArray(array);
+
+Console.WriteLine($"В массиве {String.Join(", ", array)} число четных элементов равно {CountOdd(array)}");
+
