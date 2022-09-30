af05460747112e3612c25819389520375841db5d
Koha <BelkoZdorovo@yandex.ru>
2022-09-30 08:07:24 +0300

    HW5Ex3 output and another randomizing fix

--- HomeWork5_Ex3/Program.cs
+++ HomeWork5_Ex3/Program.cs
@@ -9,13 +9,17 @@ void FillArray(double[] collection)
     int index = 0;
     while (index < collection.Length)
     {
-        collection[index] = new Random().Next(-10000, 10001) * 0.001;
-        collection[index] = Math.Round(collection[index], 2);
+
+// Решил не воевать с NextDouble из-за его сложности, поэтому заюзал целочисленную генерацию с последующим переносом запятой.
+        collection[index] = Math.Round(new Random().Next(-10000, 10001) * 0.001, 2);
         index ++;
     }
 }
 
-double DifferenceMinMax(double[] array)
+// В прошлых версиях было вычисление разницы внутри метода, что противоречило вашей рекомендации
+// "Один метод - одно действие"
+
+double[] FindMinMax(double[] array)
 {
     double max = array[0];
     double min = array[0];
@@ -31,11 +35,15 @@ double DifferenceMinMax(double[] array)
         }
     }
 
-    return Math.Round(max - min, 2); 
+    double[] result = {min, max};
+    return result;
 }
 
 double[] array = CreateArray();
 FillArray(array);
+double[] result = FindMinMax(array);
+
+double answer = Math.Round(result[1] - result[0], 2);
 
 Console.WriteLine();
-Console.WriteLine($"В массиве {String.Join(" | ", array)} разница между максимумом и минимумом равна {DifferenceMinMax(array)}");
+Console.WriteLine($"В массиве {String.Join(" | ", array)} разница между максимумом и минимумом равна {answer}");
