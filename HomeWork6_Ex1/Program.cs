8635fe318f630a69454d9ef85c358dbaaeee9b21
Koha <BelkoZdorovo@yandex.ru>
2022-10-03 23:20:12 +0300

    HW6Ex1

---
+++ HomeWork6_Ex1/Program.cs
@@ -0,0 +1,46 @@
+﻿int[] CreateArray(int num)
+{
+    int[] array = new int [num];
+    return array;
+}
+
+int[] StringToArray(string[] array)
+{
+    int[] result = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = int.Parse(array[i]);
+    }
+
+    return result;
+}
+
+int MoreThanZero(int[] array, int index = 0, int counter = 0)
+{
+    if (index < array.Length)
+    {
+        if (array[index] > 0)
+        {
+            return MoreThanZero(array, ++index, ++counter);
+        }
+        else
+        {
+            return MoreThanZero(array, ++index, counter);
+        }
+    }
+
+    return counter;
+}
+
+Console.WriteLine("Введите размерность");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Введите числа через запятую. Надо {num}");
+string[] array = Console.ReadLine()!.Split(",");
+
+int[] collection = CreateArray(num);
+collection = StringToArray(array);
+int result = MoreThanZero(collection);
+
+Console.WriteLine($"В массиве {String.Join(", ", array)} колличество чисел больше нуля равно {result}");
