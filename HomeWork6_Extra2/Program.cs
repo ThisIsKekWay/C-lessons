ca61f82f498099baf7048590643b569d3de177b5
Koha <BelkoZdorovo@yandex.ru>
2022-10-04 10:47:28 +0300

    HW6Extra2

---
+++ HomeWork6_Extra2/Program.cs
@@ -0,0 +1,15 @@
+﻿double ArMean(double sum = 0, int counter = 0)
+{
+    double num = double.Parse(Console.ReadLine()!);
+    sum += num;
+    if (num != -1)
+    {
+        counter++;
+        return ArMean(sum, counter);
+    }
+    return (sum + 1) / counter;
+}
+
+double res = Math.Round(ArMean(), 2);
+Console.WriteLine(res);
+
