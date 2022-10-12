8b8ce28a42a1874270425581282c5358c6f72873
Koha <BelkoZdorovo@yandex.ru>
2022-10-12 10:31:33 +0300

    HW8Ex4

---
+++ HomeWork8_Ex4/Program.cs
@@ -0,0 +1,37 @@
+﻿void PrintArray(int[,,] array)
+{
+    for (int i = 0; i< array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i}, {j}, {k}) ");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,,] array)
+{
+    Random generator = new Random();
+
+    for (int i = 0; i< array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = generator.Next(0, 100);
+            }
+        }
+    }
+}
+
+int[,,] collection = new int[new Random().Next(2, 4),new Random().Next(2, 4),new Random().Next(2, 4)];
+FillArray(collection);
+PrintArray(collection);
