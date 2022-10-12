33f5e556344646157eea297a8637ff8d05c3d24e
Koha <BelkoZdorovo@yandex.ru>
2022-10-12 09:16:34 +0300

    HW8Ex1

---
+++ HomeWork8_Ex1/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.Clear();
+
+int[,] CreateArray()
+{
+    int[,] result = new int[new Random().Next(2, 6), new Random().Next(2, 6)];
+    return result;
+}
+
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-5, 6);
+        }
+    }
+
+    return array;    
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        
+        Console.WriteLine();
+    }
+}
+
+// Буду признателен, если предложите рекомендации, как избежать такого большого количества вложенных циклов
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for ( int k = 0; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] <= array[i, k]) continue;
+                int buffer = array[i, j];
+                array[i, j] = array[i, k];
+                array[i, k] = buffer;
+            }
+        }
+    }
+}
+
+int[,] col = CreateArray();
+col = FillArray(col);
+PrintArray(col);
+SortArray(col);
+Console.WriteLine();
+PrintArray(col);
