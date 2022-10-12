4151ab28ec49b916186411055bea477ed2dbab44
Koha <BelkoZdorovo@yandex.ru>
2022-10-12 14:18:33 +0300

    HW8Extra2

---
+++ HomeWork8_Ex7/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+Console.Write("Введите номер строки: ");
+int num = int.Parse(Console.ReadLine()!);
+Pascal(num);
+
+void Pascal(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        int count = 1;
+        for (int j = 0; j < num - i; j++)
+        {
+            Console.Write("   ");
+        }
+ 
+        for (int x = 0; x <= i; x++)
+        {
+            Console.Write("    {0:D} ", count);
+            count = count * (i - x) / (x + 1);
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
