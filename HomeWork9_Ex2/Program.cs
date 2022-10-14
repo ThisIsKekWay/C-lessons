1da425531a5fbe517f61dd56a5c3135b1e7d2a95
Koha <BelkoZdorovo@yandex.ru>
2022-10-14 09:51:59 +0300

    HW9Ex2

---
+++ HomeWork9_Ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿int NumSum (int firstNum, int secondNum, int sum = 0)
+{
+    if (firstNum <= secondNum)
+    {
+        sum += firstNum;
+        firstNum++;
+        return NumSum(firstNum, secondNum, sum);
+    }
+
+    return sum;
+}  
+
+Console.Write("Введите первое число: ");
+int numberOne = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int numberTwo = int.Parse(Console.ReadLine()!);
+
+Console.Write(NumSum(numberOne, numberTwo));
