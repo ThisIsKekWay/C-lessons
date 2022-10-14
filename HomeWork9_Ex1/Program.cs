96e5c72d12fcf653ec7726d002fa88e03f136ae8
Koha <BelkoZdorovo@yandex.ru>
2022-10-14 08:32:41 +0300

    HWEx1

---
+++ HomeWork9_Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿void NumRow (int num)
+{
+    /* for (int i = num; i > 0; i--)
+    {
+        Console.WriteLine(num);
+        num--;
+    } */
+
+    if (num != 0)
+    {
+        Console.Write($"{num}, ");
+        NumRow(num - 1);
+    }
+}  
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+NumRow(number);
