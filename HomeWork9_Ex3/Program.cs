b09ce6fa50dcd45ecc34563ea8b34170abbe4c45
Koha <BelkoZdorovo@yandex.ru>
2022-10-14 10:16:58 +0300

    HW9Ex3

---
+++ HomeWork9_Ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿int Ackerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m != 0) && (n == 0))
+    {
+        return Ackerman(m - 1, 1);
+    }
+    else return Ackerman(m - 1, Ackerman(m, n - 1));
+
+}
+
+Console.Write("Введите m: ");
+int firstNum = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int secondNum = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Ackerman(firstNum, secondNum));
